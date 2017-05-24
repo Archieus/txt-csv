90d752b8a9cc6b541bd797eb646f0498cfab6b7c
Archieus <archieus.eng@gmail.com>
2017-05-23 21:19:54 -0400

    Initial Commit

---
+++ txt-csv.py
@@ -0,0 +1,9 @@
+import csv
+
+txt_file = r"SP500.txt" #Reades the text file to be converted
+csv_file = r"SP500.csv" #Creates Output csv File Name to write to
+
+in_txt = csv.reader(open(txt_file, 'r'), delimiter = ",") #Converts txt file to csv format
+out_csv = csv.writer(open(csv_file, 'w', newline = '')) #writes data to csv file deleting line spaces
+
+out_csv.writerows(in_txt)
